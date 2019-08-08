14f5b71e0681729633d08a81b413653715420cd3
Bryan Alvarenaga <bryanalvarenaga@bryans-mbp.attlocal.net>
2019-08-08 13:53:56 -0400

    created dashboard and homepage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomepageComponent } from './homepage/homepage/homepage.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
