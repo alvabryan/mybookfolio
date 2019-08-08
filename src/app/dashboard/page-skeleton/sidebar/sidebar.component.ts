9477be701e6a75e0c02704c04dd7ff9bc497e81d
Bryan Alvarenaga <bryanalvarenaga@bryans-mbp.attlocal.net>
2019-08-08 15:31:21 -0400

    implemented toggle button

--- src/app/dashboard/page-skeleton/sidebar/sidebar.component.ts
+++ src/app/dashboard/page-skeleton/sidebar/sidebar.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ToggleSideBarService } from '../toggle-side-bar.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.css']
 })
-export class SidebarComponent implements OnInit {
+export class SidebarComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  toggledBar = false;
+  subscription: Subscription;
+
+  constructor(private toggleSideBar: ToggleSideBarService) {
+    this.subscription = this.toggleSideBar.toggleBar.subscribe(toggle => {
+
+      if (toggle === this.toggledBar) {
+        this.toggledBar = false;
+      } else {
+        this.toggledBar = true;
+      }
+    });
+   }
 
   ngOnInit() {
+
   }
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
+
 
 }
