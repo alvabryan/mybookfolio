162167caea32cb4d89929f0a1d2cbd7b9646f4c9
Bryan Alvarenga <bryanalvarenga@bryans-mbp.attlocal.net>
2020-02-15 21:41:02 -0500

    file upload in progress

--- src/app/portfolio/course-work/file-upload/file-upload.component.ts
+++ src/app/portfolio/course-work/file-upload/file-upload.component.ts
@@ -1,17 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy, ViewChild } from '@angular/core';
 import { NgForm, FormGroup, FormControl } from '@angular/forms';
+
+// ngrx
+import { Store } from '@ngrx/store';
+import * as fromPortfolio from '../../store/index';
+import * as PortfolioActions from '../../store/portfolio.actions';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-file-upload',
   templateUrl: './file-upload.component.html',
   styleUrls: ['./file-upload.component.css']
 })
-export class FileUploadComponent implements OnInit {
+export class FileUploadComponent implements OnInit, OnDestroy {
+
+  subscription: Subscription = new Subscription();
 
   fileUploadForm: FormGroup;
   fileData: any;
+  loading = false;
+  isModalShow = false;
 
-  constructor() { }
+  constructor(private store: Store<fromPortfolio.PortfolioState>) { }
 
   ngOnInit() {
     this.fileUploadForm = new FormGroup({
@@ -25,8 +35,24 @@ export class FileUploadComponent implements OnInit {
   }
 
   onSubmit() {
-    console.log(this.fileUploadForm.value);
-    console.log(this.fileData);
+    this.loading = true;
+    const formData = this.fileUploadForm.value;
+    this.store.dispatch(PortfolioActions.uploadFile({
+      fileName: formData.fileName,
+      file: this.fileData,
+      description: formData.description
+    }));
+
+    this.subscription.add(
+      this.store.select('portfolio').subscribe((data: any) => {
+        this.loading = data.uploading;
+        this.isModalShow = true;
+      })
+    );
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 
 }
