693503491c053b81b6c67c22e727a0486af73c02
Bryan Alvarenga <bryanalvarenga@bryans-mbp.attlocal.net>
2020-03-20 22:56:49 -0400

    financial planning m1 m2 m3 started

--- src/app/portfolio/financial-planning-module5/financial-planning-module5.component.ts
+++ src/app/portfolio/financial-planning-module5/financial-planning-module5.component.ts
@@ -7,9 +7,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FinancialPlanningModule5Component implements OnInit {
 
+  pageIndex = 1;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+
+  onClickBack() {
+    if (this.pageIndex === 1) {
+      return ;
+    } else {
+      this.pageIndex -= 1;
+    }
+  }
+
+  onClickNext() {
+    if (this.pageIndex === 40) {
+      return ;
+    } else {
+      this.pageIndex += 1;
+    }
+  }
+
+  onInputChange(event) {
+    const value = +event.target.value;
+    if (value > 0 && value <= 40) {
+      this.pageIndex = value;
+    } else {
+      this.pageIndex = 1;
+    }
+
   }
 
 }
