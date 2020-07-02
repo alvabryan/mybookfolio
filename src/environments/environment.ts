38d3d3a5e1d21249b563151b9bc654557628efef
Bryan Alvarenga <bryanalvarenga@bryans-mbp.attlocal.net>
2020-07-02 18:31:00 -0400

    google analytics started

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -11,7 +11,8 @@ export const environment = {
     projectId: 'mybookfolio-acc55',
     storageBucket: 'mybookfolio-acc55.appspot.com',
     messagingSenderId: '326193354984',
-    appId: '1:326193354984:web:0b915fe974e7d66f'
+    appId: '1:326193354984:web:0b915fe974e7d66f',
+    measurementId: 'G-WDLQKRGLRF'
   }
 };
 
