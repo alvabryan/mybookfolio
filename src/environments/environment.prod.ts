883ff99c451c6c12cfbf3d1655a6d696b67916ec
Bryan Alvarenga <bryanalvarenga@bryans-mbp.attlocal.net>
2020-07-02 20:20:55 -0400

    app deployed

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyBlWRtrgBndlJ8Pok--wJzyOLlkz6FTm-Q',
+    authDomain: 'mybookfolio-acc55.firebaseapp.com',
+    databaseURL: 'https://mybookfolio-acc55.firebaseio.com',
+    projectId: 'mybookfolio-acc55',
+    storageBucket: 'mybookfolio-acc55.appspot.com',
+    messagingSenderId: '326193354984',
+    appId: '1:326193354984:web:0b915fe974e7d66f',
+    measurementId: 'G-WDLQKRGLRF'
+  }
 };
