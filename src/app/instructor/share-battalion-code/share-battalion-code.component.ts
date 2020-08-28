41fc6c17d6d479a2e26c9733d4a03c909c2172dd
Bryan Alvarenga <bryanalvarenga@bryans-mbp.attlocal.net>
2020-08-28 16:20:09 -0400

    homepage completed

--- src/app/instructor/share-battalion-code/share-battalion-code.component.ts
+++ src/app/instructor/share-battalion-code/share-battalion-code.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Store } from '@ngrx/store';
+
+import * as fromInstructor from '../store/index';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-share-battalion-code',
   templateUrl: './share-battalion-code.component.html',
   styleUrls: ['./share-battalion-code.component.css']
 })
-export class ShareBattalionCodeComponent implements OnInit {
+export class ShareBattalionCodeComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  subscription: Subscription = new Subscription();
+  battalionCode = null;
+
+  constructor(private store: Store<fromInstructor.State>) { }
 
   ngOnInit() {
+    this.store.select('auth').subscribe((data: any) => {
+      if (data) {
+        this.battalionCode = data.user.battalionCode;
+      }
+    });
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 
 }
